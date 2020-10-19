61815a68af7d847e15e681660fb4f8f47f808c88
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-19 09:16:07 -0500

    fixing mailer

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,7 +14,7 @@ function apiRouter(app){
                 secure: false,
                 auth:{
                     user: "daysloth.mailsend@gmail.com",
-                    pass: "JB0GACNvIcda5Ds7",
+                    pass: "xsmtpsib-cf25c8fad4eadc2c5d8a2293ba78b44c4eede1ae6733a02e89ecbd3e0bf4feeb-TbQ1cW0OjPBdVY8k",
                 }
             });
 

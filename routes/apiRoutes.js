6f4c9ba86fb30f2bf322932d8cb674e9b8e3aff0
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-19 13:51:40 -0500

    fixing mailing router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,17 +9,17 @@ function apiRouter(app){
 
         async function sendMail(){
             let transporter = nodemailer.createTransport({
-                host: "smtp.gmail.com",
+                host: "smtp.sendgrid.net",
                 port: 587,
                 secure: false,
                 auth:{
-                    user: "daysloth.mailsend@gmail.com",
-                    pass: "pbdltrpbqxpwjdvp",
+                    user: "apikey",
+                    pass: "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",
                 }
             });
 
             let info = await transporter.sendMail({
-                from: "Daysloth Mailer <daysloth.sendmail@gmail.com>",
+                from: "Daysloth Mailer",
                 to: "allisterrampenthal@gmail.com",
                 subject: `[Daysloth Webpage response]`,
                 html: emailHTMLGenerator(emailObj)

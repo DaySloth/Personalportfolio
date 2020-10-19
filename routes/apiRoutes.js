9d56d477cdde0faf3eb88ae0d9190efcfbb9f534
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-19 14:12:54 -0500

    attempting to get mailing to work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,33 +2,27 @@ const nodemailer = require("nodemailer");
 const { mainModule } = require("process");
 const emailHTMLGenerator = require("../template/emailGenerator");
 
-function apiRouter(app){
-    app.post("/api/email", function(req, res){
+function apiRouter(app) {
+    app.post("/api/email", function (req, res) {
         const emailObj = req.body;
+        console.log(process.env.SENDGRID_API_KEY);
 
-
-        async function sendMail(){
-            let transporter = nodemailer.createTransport({
-                host: "smtp.sendgrid.net",
-                port: 587,
-                secure: false,
-                auth:{
-                    user: "apikey",
-                    pass: "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",
-                }
-            });
-
-            let info = await transporter.sendMail({
-                from: "Daysloth Mailer",
-                to: "allisterrampenthal@gmail.com",
-                subject: `[Daysloth Webpage response]`,
-                html: emailHTMLGenerator(emailObj)
-            });
-
-            console.log("Email Sent");
+        const sgMail = require('@sendgrid/mail')
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+        const msg = {
+            to: 'arampenthal@outlook.com',
+            from: 'app188086466@heroku.com',
+            subject: '[Daysloth Webpage Response]',
+            html: emailHTMLGenerator(emailObj),
         }
-
-        sendMail().catch(console.error);
+        sgMail
+            .send(msg)
+            .then(() => {
+                console.log('Email sent')
+            })
+            .catch((error) => {
+                console.error(error)
+            })
     })
 };
 

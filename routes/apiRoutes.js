c0ab18bdfc415b25e40da82d13a1a8b7b7bab0bf
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-20 08:49:06 -0500

    trying to fix mailer

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,19 @@
-const nodemailer = require("nodemailer");
-const { mainModule } = require("process");
 const emailHTMLGenerator = require("../template/emailGenerator");
+const sendmail = require('sendmail')();
 
 function apiRouter(app) {
     app.post("/api/email", function (req, res) {
         const emailObj = req.body;
-        console.log(process.env.SENDGRID_API_KEY);
 
-        const sgMail = require('@sendgrid/mail')
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-        const msg = {
-            to: 'arampenthal@outlook.com',
-            from: 'app188086466@heroku.com',
-            subject: '[Daysloth Webpage Response]',
+        sendmail({
+            from: 'arampenthal@outlook.com',
+            to: 'allisterrampenthal@gmail.com',
+            subject: 'test sendmail',
             html: emailHTMLGenerator(emailObj),
-        }
-        sgMail
-            .send(msg)
-            .then(() => {
-                console.log('Email sent')
-            })
-            .catch((error) => {
-                console.error(error)
-            })
+        }, function (err, reply) {
+            console.log(err && err.stack);
+            console.dir(reply);
+        });
     })
 };
 
